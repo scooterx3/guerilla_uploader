62344b7e36eaa395d57cf879dead57210a829259
Riley <riley.loader@gmail.com>
2014-06-20 15:01:19 -0600

    fixed bash to get correct file, updated post_max_size

--- insert_guerilla.sh
+++ insert_guerilla.sh
@@ -3,10 +3,10 @@
 mkdir guerilla_uploader 
 cd guerilla_uploader
 
-wget https://raw.githubusercontent.com/scooterx3/guerilla_uploader/master/guerilla_uploader.php
+wget https://raw.githubusercontent.com/scooterx3/guerilla_uploader/master/index.php
 
 echo "
-post_max_size = 10M;
+post_max_size = 10G;
 max_execution_time = 3600;
 max_file_uploads = 25;
 memory_limit = 256M;
