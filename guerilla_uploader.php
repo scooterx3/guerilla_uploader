ec49056eb8d181b93a855768414c08f9bc0f1b76
Riley <riley.loader@gmail.com>
2014-06-20 14:51:20 -0600

    single-file php and added bash script

--- guerilla_uploader.php
+++ guerilla_uploader.php
@@ -1,19 +1,12 @@
 <?php
-ini_set('upload_max_filesize','10737418240'); //full byte length
-ini_set('memory_limit','10737418240');
-ini_set('max_file_uploads','25');
-ini_set('post_max_size','10737418240');
-ini_set('max_execution_time','3600');
-
-
 
 function incrementFileName($filename){
 	$file_ext = '.'.end(explode(".", $filename));
 	$root_name = str_replace(($file_ext),"",$filename);
-	$file = $target_path.$filename;
+	$file = $filename;
 	$i = 1;
 	while(file_exists($file)){
-		$file = $target_path.$root_name." ($i)".$file_ext;
+		$file = $root_name." ($i)".$file_ext;
 		$i++;
 	}
 	return $file;
@@ -26,28 +19,40 @@ function seppuku(){
 	unlink(__FILE__);
 }
 
-$target_path = "files/";
+function goBabyGo(){
+	for ($i = 0; $i <count($_FILES['uploadedfile']['name']); $i++) {
+		// print_r($_FILES['uploadedfile']['name'][$i].'<br>');
+		$file_base = basename($_FILES['uploadedfile']['name'][$i]);
+		$target_file = $file_base; 
 
-for ($i = 0; $i <count($_FILES['uploadedfile']['name']); $i++) {
-	// print_r($_FILES['uploadedfile']['name'][$i].'<br>');
-	$file_base = basename($_FILES['uploadedfile']['name'][$i]);
-	$target_file = $target_path . $file_base; 
+		if (file_exists($target_file)){
+			echo $target_file." exists!";
+			$target_file = incrementFileName($target_file);
+		}
 
-	if (file_exists($target_file)){
-		echo $target_file." exists!";
-		$target_file = incrementFileName($target_file);
+		if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'][$i], $target_file)) {
+		    echo "The file ".  $target_file.   " has been uploaded<br>";
+		} else{
+			echo $i;
+			echo $_FILES['uploadedfile']['name'][$i];
+		    echo "There was an error uploading the file, please try again!";
+		}
 	}
-
-	if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'][$i], $target_file)) {
-	    echo "The file ".  $target_file.   " has been uploaded<br>";
-	} else{
-		echo $i;
-		echo $_FILES['uploadedfile']['name'][$i];
-	    echo "There was an error uploading the file, please try again!";
-	}
-
-
+	seppuku();
 }
 
-seppuku();
+if (isset($_POST['guerilla_uploader'])){
+
+goBabyGo();
+
+}else{
+	?>
+		<form enctype="multipart/form-data" action="guerilla_uploader.php" method="POST">
+		<input type="hidden" name="guerilla_uploader" />
+		Choose a file to upload: <input name="uploadedfile[]" type="file" multiple='multiple' /><br />
+		<input type="submit" value="Upload File" />
+		</form>
+	<?php
+}
+
 ?>
---
+++ insert_guerilla.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir guerilla_uploader && cd guerilla_uploader
+wget https://raw.githubusercontent.com/scooterx3/guerilla_uploader/master/guerilla_uploader.php
+echo "
+post_max_size = 10M;
+max_execution_time = 3600;
+max_file_uploads = 25;
+memory_limit = 256M;
+upload_max_filesize = 10G;" > php.ini
+
