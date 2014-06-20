b307536c79d28746c821d292108d9baed5d17ed6
Riley <riley.loader@gmail.com>
2014-06-20 15:04:24 -0600

    refined bash script

--- insert_guerilla.sh
+++ insert_guerilla.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-mkdir guerilla_uploader 
-cd guerilla_uploader
+destination = 'guerilla_uploader';
 
-wget https://raw.githubusercontent.com/scooterx3/guerilla_uploader/master/index.php
+mkdir $destination 
+
+wget -P $destination https://raw.githubusercontent.com/scooterx3/guerilla_uploader/master/index.php
 
 echo "
 post_max_size = 10G;
 max_execution_time = 3600;
 max_file_uploads = 25;
 memory_limit = 256M;
-upload_max_filesize = 10G;" > php.ini
+upload_max_filesize = 10G;" > $destination/php.ini
 
